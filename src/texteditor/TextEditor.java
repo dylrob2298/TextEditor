5a0f82a59178c84ac4fba86d95957d166b12a11a
dylrob2298 <dylrob123@gmail.com>
2017-03-01 22:49:10 -0500

    File menu bar has all components, working on saving and loading .txt files

--- src/texteditor/TextEditor.java
+++ src/texteditor/TextEditor.java
@@ -3,6 +3,8 @@ package texteditor;
 import java.awt.BorderLayout;
 import java.awt.event.*;
 
+import java.io.*;
+
 import javax.swing.*;
 
 public class TextEditor extends JFrame{
@@ -12,9 +14,12 @@ public class TextEditor extends JFrame{
   private JTextArea text;
   private JScrollPane jScrollPanel;
   private JMenuBar menuBar;
+  private String currentFile;
   
   public TextEditor() {
     super("TextEditor");
+    
+    currentFile = "Untitled";
     
     initUI();
     
@@ -24,19 +29,48 @@ public class TextEditor extends JFrame{
   private void initUI() {
     
     menuBar = new JMenuBar();
-    ImageIcon icon = new ImageIcon("exit.png");
     
     JMenu file = new JMenu("File");
     file.setMnemonic(KeyEvent.VK_F);
+    
+    
+    // open file button
+    JMenuItem openMI = new JMenuItem("Open");
+    openMI.setMnemonic(KeyEvent.VK_O);
+    openMI.setToolTipText("Open a new .txt file");
+    
+    
+    // save file button
+    JMenuItem saveMI = new JMenuItem("Save");
+    saveMI.setMnemonic(KeyEvent.VK_S);
+    saveMI.setToolTipText("Save this file");
+    
+    
+    // save as file button
+    JMenuItem saveAsMI = new JMenuItem("Save as");
+    saveAsMI.setMnemonic(KeyEvent.VK_A);
+    saveAsMI.setToolTipText("Save this as a new file");
+    
+    
+    // new file button
+    JMenuItem newMI = new JMenuItem("New");
+    newMI.setMnemonic(KeyEvent.VK_N);
+    newMI.setToolTipText("Create a new .txt file");
+    
 
     // exit application button
-    JMenuItem exitMI = new JMenuItem("Exit", icon);
+    JMenuItem exitMI = new JMenuItem("Exit");
     exitMI.setMnemonic(KeyEvent.VK_E);
     exitMI.setToolTipText("Exit application");
     exitMI.addActionListener((ActionEvent event) -> {
         System.exit(0);
     });
     
+    
+    file.add(openMI);
+    file.add(saveMI);
+    file.add(saveAsMI);
+    file.add(newMI);
     file.add(exitMI);
 
     menuBar.add(file);
@@ -49,6 +83,42 @@ public class TextEditor extends JFrame{
     jScrollPanel = new JScrollPane(text);
     add(jScrollPanel, BorderLayout.CENTER);
     pack();
+    
+  }
+  
+  
+  private void loadFile(String filename) {
+    
+    try {
+      FileReader r = new FileReader(filename);
+      text.read(r, null);
+      r.close();
+      currentFile = filename;
+      
+      
+    }
+    catch (IOException e) {
+      
+      
+    }
+    
+    
+  }
+  
+  private void saveFile(String filename) {
+    
+    try {
+      FileWriter w = new FileWriter(filename);
+      text.write(w);
+      w.close();
+      currentFile = filename;
+      
+      
+    }
+    
+    catch (IOException e) {
+      
+    }
     
   }
   
